bedad309503faaaa5305e437f845f32a6adec3bf
Sergii <sergii.web@gmail.com>
2023-12-21 21:44:44 +0200

    part1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CustomConfigModule } from './config/config.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      useFactory: () => {
+        return {
+          type: 'mysql',
+          host: 'http://owu.linkpc.net',
+          port: 3306,
+          username: 'progschool',
+          password: 'progschool',
+          database: 'progschool',
+          entities: [],
+          synchronize: true,
+        };
+      },
+      inject: [],
+    }),
+    UsersModule,
+    AuthModule,
+    CustomConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
